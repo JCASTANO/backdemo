dad418bd51e78393df09682f0c93a42fc4588a4d
alvaro.osorio <alvaro.osorio@ALVARO-OSORIO.ceiba.local>
2019-03-01 08:38:47 -0500

    Implementacion end-point para revocar tokens /revoke-token

--- ZuulProxy/src/main/java/com/demo/seguridad/ResourceServerConfiguration.java
+++ ZuulProxy/src/main/java/com/demo/seguridad/ResourceServerConfiguration.java
@@ -2,11 +2,17 @@ package com.demo.seguridad;
 
 
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,7 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 {
-	
+	@Autowired 
+	ConsumerTokenServices tokenServices;
+	 
 	@RequestMapping("/publica")
 	public String publico() {
 		return "Pagina Publica";
@@ -28,22 +36,32 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 	public String admin() {
 		return "Pagina Administrador";
 	}
+	
+	
+	@DeleteMapping("/revoke-token")
+	@ResponseBody
+	public void revocarToken(HttpServletRequest request){
+		
+		String authorization = request.getHeader("Authorization");
+		if (authorization != null && authorization.contains("Bearer")) {
+			String tokenId = authorization.substring("Bearer".length() + 1);
+			tokenServices.revokeToken(tokenId);
+		}
+
+	}
+	
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http
-		.authorizeRequests().antMatchers("/oauth/token", "/oauth/authorize**", "/publica", "/customer/pelicula/**").permitAll();
+		.authorizeRequests().antMatchers("/oauth/token", "/oauth/authorize**", "/publica").permitAll();
 		
 		http.requestMatchers()
-		
-//		.antMatchers("/customer/pelicula/**")
-//		.and().authorizeRequests()
-//		.antMatchers("/customer/pelicula/**").access("hasRole('USER')")
-		
+			
 		.and().requestMatchers()
-		.antMatchers("/privada")
+		.antMatchers("/privada","/customer/peliculas","/revoke-token")
 		.and().authorizeRequests()
-		.antMatchers("/privada").access("hasRole('USER')")
+		.antMatchers("/privada","/customer/peliculas","/revoke-token").access("hasRole('USER')")
 		
 		.and().requestMatchers().antMatchers("/admin")
 		.and().authorizeRequests()
