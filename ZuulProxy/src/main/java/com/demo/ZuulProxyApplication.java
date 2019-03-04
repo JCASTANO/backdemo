932da7ddaccf0bdf187cace444d8d08174a3f282
alvaro.osorio <alvaro.osorio@ALVARO-OSORIO.ceiba.local>
2019-03-04 17:52:57 -0500

    Ajustes crossOrigin

--- ZuulProxy/src/main/java/com/demo/ZuulProxyApplication.java
+++ ZuulProxy/src/main/java/com/demo/ZuulProxyApplication.java
@@ -2,33 +2,31 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableWebSecurity
+@CrossOrigin("*")
 public class ZuulProxyApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulProxyApplication.class, args);
 	}
 	
-
 	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
@@ -54,7 +52,9 @@ public class ZuulProxyApplication extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+        	.csrf().disable()
+            .authorizeRequests()
             	.antMatchers("/","/index","/webpublico").permitAll()
             	.antMatchers("/webprivado").authenticated()
             	.antMatchers("/webadmin").hasRole("ADMIN").and()
@@ -65,5 +65,6 @@ public class ZuulProxyApplication extends WebSecurityConfigurerAdapter {
                 .logout() // Metodo get pues he desabilitado CSRF
                 .permitAll();
     }
-
+	
+	
 }
--- ZuulProxy/src/main/java/com/demo/seguridad/ResourceServerConfiguration.java
+++ ZuulProxy/src/main/java/com/demo/seguridad/ResourceServerConfiguration.java
@@ -5,6 +5,7 @@ package com.demo.seguridad;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -16,14 +17,14 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http
-		.authorizeRequests().antMatchers("/oauth/token", "/oauth/authorize**", "/publica").permitAll();
+		.authorizeRequests().antMatchers("/oauth/token", "/oauth/authorize**", "/publica","/revoke-token").permitAll();
 		
 		http.requestMatchers()
 			
 		.and().requestMatchers()
-		.antMatchers("/privada","/customer/peliculas","/revoke-token")
+		.antMatchers("/privada","/customer/peliculas")
 		.and().authorizeRequests()
-		.antMatchers("/privada","/customer/peliculas","/revoke-token").access("hasRole('USER')")
+		.antMatchers("/privada","/customer/peliculas").access("hasRole('USER')")
 		
 		.and().requestMatchers().antMatchers("/admin")
 		.and().authorizeRequests()
